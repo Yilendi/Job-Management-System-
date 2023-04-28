62784ab4abf6c92f3d4c98048d67565279efaf2c
Yilendi <yilenda001@gmail.com>
2023-04-27 22:45:09 -0400

    feat: mysql

--- jobBoard/src/main/java/com/yilendaLevra/jobBoard/JobService.java
+++ jobBoard/src/main/java/com/yilendaLevra/jobBoard/JobService.java
@@ -6,30 +6,40 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 @Service
 public class JobService {
     @Autowired
     private JobRepository jobRepository;
 
+    private static final String CSV_FILE_PATH = "src/a.csv";
+
     public void importJobsFromCsvFile(String csvFilePath) throws IOException {
-        BufferedReader csvReader = new BufferedReader(new FileReader(csvFilePath));
-        String row;
-        boolean firstLine = true;
-        // need to skip header line
-        while ((row = csvReader.readLine()) != null) {
-            if (firstLine) {
-                firstLine = false;
-                continue;
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                String occupation = values[9];
+                String occupationLevel = values[10];
+                int annualSalary = Integer.parseInt(values[18]);
+                saveToMysql(occupation, occupationLevel, annualSalary);
             }
-            String[] data = row.split(",");
-            Job job = new Job();
-            job.setOccupation(data[9]);
-            job.setOccupationLevel(data[10]);
-            job.setAnnualSalary(Integer.parseInt(data[18]));
-            jobRepository.save(job);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        csvReader.close();
+    }
+
+    private static void saveToMysql(String occupation, String occupationLevel, int annualSalary) throws Exception {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase?user=root&password=mypassword");
+             PreparedStatement stmt = conn.prepareStatement("INSERT INTO jobs (occupation, occupation_level, annual_salary) VALUES (?, ?, ?)")) {
+            stmt.setString(1, occupation);
+            stmt.setString(2, occupationLevel);
+            stmt.setInt(3, annualSalary);
+            stmt.executeUpdate();
+        }
     }
 
 }
